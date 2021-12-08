ca0ed1ab70edc7a14b436f0c7c9e0b4bacddb7d5
Dirk Zimmermann <me@dirkz.com>
2021-12-08 11:14:24 +0100

    Can show times

--- src/RussianClock/Util/TimeStruct.purs
+++ src/RussianClock/Util/TimeStruct.purs
@@ -1,9 +1,26 @@
 module RussianClock.Util.TimeStruct
   ( TimeStruct
-  )
-  where
+  , timeStructString
+  ) where
+
+import Prelude
+import Data.String (length)
 
 type TimeStruct
   = { hour :: Int
     , minute :: Int
     }
+
+timeStructString ::
+  forall r.
+  { hour :: Int, minute :: Int
+  | r
+  } ->
+  String
+timeStructString time = nshow time.hour <> ":" <> nshow time.minute
+  where
+  nullPrefix str
+    | length str == 1 = "0" <> str
+    | otherwise = str
+
+  nshow = show >>> nullPrefix
