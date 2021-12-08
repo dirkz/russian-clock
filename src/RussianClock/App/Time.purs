e7d79fd618b65ee0aceb0c2c2bb02d5c4bd11a32
Dirk Zimmermann <me@dirkz.com>
2021-12-08 11:41:04 +0100

    Try to get the voices

--- src/RussianClock/App/Time.purs
+++ src/RussianClock/App/Time.purs
@@ -3,6 +3,7 @@ module RussianClock.App.Time
   ) where
 
 import Prelude
+import Data.Array (filter, (!!))
 import Data.Int (round)
 import Data.Maybe (Maybe(..), fromMaybe)
 import Effect.Class (class MonadEffect)
@@ -10,9 +11,12 @@ import Effect.Random (random)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import RussianClock.Util.TimeStruct (TimeStruct, timeStructString)
+import Web.HTML (window)
 import Web.Speech.TTS as TTS
 import Web.Speech.TTS.Voice as V
+import Effect.Aff.Class (class MonadAff)
 
 unknown :: String
 unknown = "unknown"
@@ -24,16 +28,23 @@ type State
   = { maybeTime :: Maybe TimeStruct
     , voices :: Array V.Voice
     , maybeVoice :: Maybe V.Voice
+    , maybeError :: Maybe String
     }
 
 data Action
   = Initialize
   | Random
 
-component :: forall q i o m. MonadEffect m => H.Component q i o m
+component :: forall q i o m. MonadEffect m => MonadAff m => H.Component q i o m
 component =
   H.mkComponent
-    { initialState: \_ -> { maybeTime: Nothing, voices: [], maybeVoice: Nothing }
+    { initialState:
+        \_ ->
+          { maybeTime: Nothing
+          , voices: []
+          , maybeVoice: Nothing
+          , maybeError: Nothing
+          }
     , render
     , eval:
         H.mkEval
@@ -48,6 +59,9 @@ render st =
   HH.article_
     [ HH.h1_ [ HH.text "Russian Time" ]
     , HH.p_ [ HH.text $ fromMaybe unknown $ timeStructString <$> st.maybeTime ]
+    , case st.maybeError of
+        Nothing -> HH.text ""
+        Just err -> HH.p [ HP.classes [ HH.ClassName "error" ] ] [ HH.text err ]
     , HH.p_
         [ HH.button
             [ HE.onClick \_ -> Random ]
@@ -55,9 +69,20 @@ render st =
         ]
     ]
 
-handleAction :: forall cs o m. MonadEffect m => Action → H.HalogenM State Action cs o m Unit
+handleAction :: forall cs o m. MonadEffect m => MonadAff m => Action → H.HalogenM State Action cs o m Unit
 handleAction = case _ of
-  Initialize -> handleAction Random
+  Initialize -> do
+    handleAction Random
+    w <- H.liftEffect window
+    maybeSynth <- H.liftEffect $ TTS.tts w
+    case maybeSynth of
+      Nothing -> H.modify_ \st -> st { maybeError = Just "No TTS support" }
+      Just synth -> do
+        voices <-
+          map (map (filter (\v -> V.lang v == language)))
+            H.liftAff
+            $ TTS.voices synth
+        H.modify_ \st -> st { voices = voices, maybeVoice = voices !! 0 }
   Random -> do
     rh <- H.liftEffect random
     rm <- H.liftEffect random
